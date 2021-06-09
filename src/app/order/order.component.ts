0286cfadd5aae1c8a3f125ef6acf70ef0c143a58
José Carlos Rodrigues Lopes <jcarlosrl22@gmail.com>
2021-06-09 17:22:40 -0300

    Implementação da aplicação. Carrinho de compras, avaliação, tela de pedidos

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { CartItem } from 'app/restaurant-detail/shopping-cart/cart-item.model';
 import { RadioOption } from 'app/shared/radio/radio-option.model';
+import { Order, OrderItem } from './order.model';
 import { OrderService } from './order.service';
 
 @Component({
@@ -17,7 +19,7 @@ export class OrderComponent implements OnInit {
 
   delivery: number = 8;
 
-  constructor(private orderService: OrderService) { }
+  constructor(private orderService: OrderService, private router: Router) { }
 
   ngOnInit() {
   }
@@ -40,6 +42,16 @@ export class OrderComponent implements OnInit {
 
   remove(item: CartItem) {
     this.orderService.remove(item);
+  }
+
+  checkOrder(order: Order) {
+    order.orderItems = this.cartItems()
+      .map((cartItem: CartItem) => new OrderItem(cartItem.quantity, cartItem.menuItem.id));  
+
+    this.orderService.checkOrder(order).subscribe((orderId: string) => {
+      this.router.navigate(['/order-summary'])
+      this.orderService.clear();
+    });
   }
 
 }
--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from "@angular/core";
+import { Headers, Http, RequestOptions } from "@angular/http";
+import { MEAP_API } from "app/app.api";
 import { CartItem } from "app/restaurant-detail/shopping-cart/cart-item.model";
 import { ShoppingCartService } from "app/restaurant-detail/shopping-cart/shopping-cart.service";
+import { Observable } from "rxjs";
+import { Order, OrderItem } from "./order.model";
 
 @Injectable()
 export class OrderService {
 
-  constructor(private cartService: ShoppingCartService){}
+  constructor(private cartService: ShoppingCartService, private http: Http){}
 
   cartItems(): CartItem[] {
     return this.cartService.items;
@@ -25,6 +29,18 @@ export class OrderService {
 
   remove(item: CartItem) {
     this.cartService.removeItem(item);
+  }
+
+  checkOrder(order: Order): Observable<string> {
+    const headers = new Headers()
+    headers.append('Content-Type', 'application/json')
+    return this.http.post(`${MEAP_API}/orders`, JSON.stringify(order), new RequestOptions({headers: headers}))
+      .map(response => response.json())
+      .map(order => order.id);
+  }
+
+  clear() {
+    this.cartService.clear();
   }
 
 }
