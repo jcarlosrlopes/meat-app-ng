bcb76295b88b0d16152c77fb3cc1b512f019725f
José Carlos Rodrigues Lopes <jcarlosrl22@gmail.com>
2021-06-10 12:11:49 -0300

    Separação da aplicação em módulos

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,25 @@
+import { CommonModule } from "@angular/common";
+import { ModuleWithProviders, NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { OrderService } from "app/order/order.service";
+import { ShoppingCartService } from "app/restaurant-detail/shopping-cart/shopping-cart.service";
+import { RestaurantsService } from "app/restaurants/restaurants.service";
+import { InputComponent } from "./input/input.component";
+import { RadioComponent } from "./radio/radio.component";
+import { RatingComponent } from "./rating/rating.component";
+
+@NgModule({
+  declarations: [ InputComponent, RadioComponent, RatingComponent ],
+  imports: [ CommonModule, FormsModule ],
+  exports: [ InputComponent, RadioComponent, RatingComponent, CommonModule, FormsModule ]
+})
+export class SharedModule {
+
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: SharedModule,
+      providers: [ RestaurantsService, ShoppingCartService, OrderService ]
+    }    
+  }
+
+}
