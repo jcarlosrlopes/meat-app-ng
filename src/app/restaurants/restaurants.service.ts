61a03d54de9843cb6acf5e508b10c30b5d573885
José Carlos Rodrigues Lopes <jcarlosrl22@gmail.com>
2023-07-30 17:25:13 -0300

    Busca de restaurantes e uso de operadores do rxjs

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -1,40 +1,42 @@
-import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import { MEAP_API } from 'app/app.api';
-import { ErrorHandler } from 'app/app.error-handler';
-import { Observable } from 'rxjs';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/catch';
-import { Restaurant } from './restaurant/restaurant.model';
-import { MenuItem } from 'app/restaurant-detail/menu-item/menu-item.model';
+import { Injectable } from "@angular/core";
+import { Http } from "@angular/http";
+import { MEAP_API } from "app/app.api";
+import { ErrorHandler } from "app/app.error-handler";
+import { Observable } from "rxjs";
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/catch";
+import { Restaurant } from "./restaurant/restaurant.model";
+import { MenuItem } from "app/restaurant-detail/menu-item/menu-item.model";
 
 @Injectable()
-export class RestaurantsService {  
+export class RestaurantsService {
+  constructor(private http: Http) {}
 
-  constructor(private http: Http) { }
-
-  restaurants(): Observable<Restaurant[]> {
-    return this.http.get(`${MEAP_API}/restaurants`)
-    .map(response => response.json())
-    .catch(ErrorHandler.handleError);
+  restaurants(search?: string): Observable<Restaurant[]> {
+    return this.http
+      .get(`${MEAP_API}/restaurants`, { params: { q: search } })
+      .map((response) => response.json())
+      .catch(ErrorHandler.handleError);
   }
 
   restaurantById(id: string): Observable<Restaurant> {
-    return this.http.get(`${MEAP_API}/restaurants/${id}`)
-    .map(response => response.json())
-    .catch(ErrorHandler.handleError);
+    return this.http
+      .get(`${MEAP_API}/restaurants/${id}`)
+      .map((response) => response.json())
+      .catch(ErrorHandler.handleError);
   }
 
   reviewsOfRestaurant(id: string): Observable<any> {
-    return this.http.get(`${MEAP_API}/restaurants/${id}/reviews`)
-    .map(response => response.json())
-    .catch(ErrorHandler.handleError);
+    return this.http
+      .get(`${MEAP_API}/restaurants/${id}/reviews`)
+      .map((response) => response.json())
+      .catch(ErrorHandler.handleError);
   }
 
   menuOfRestaurant(id: string): Observable<MenuItem[]> {
-    return this.http.get(`${MEAP_API}/restaurants/${id}/menu`)
-    .map(response => response.json())
-    .catch(ErrorHandler.handleError);
+    return this.http
+      .get(`${MEAP_API}/restaurants/${id}/menu`)
+      .map((response) => response.json())
+      .catch(ErrorHandler.handleError);
   }
-
 }
